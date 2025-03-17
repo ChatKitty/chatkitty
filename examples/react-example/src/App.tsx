a1d0f237648b4569a00b9f913e33f72d0d4880c9
Kevin Grafstrom <kevin.grafstrom@gmail.com>
2025-03-17 15:07:53 -0400

    wip(react): allow apiKey, username, and widgetId to be passed in

--- examples/react-example/src/App.tsx
+++ examples/react-example/src/App.tsx
@@ -6,7 +6,8 @@ import '@chatkitty/react/dist/react.css'
 function App() {
   return (
     <>
-        <ChatUi/>
+        {/*<ChatUi/>*/}
+        <ChatUi apiKey="afaac908-1db3-4b5c-a7ae-c040b9684403" username="2989c53a-d0c5-4222-af8d-fbf7b0c74ec6" widgetId="UWiEkKvdAaUJ1xut"/>
     </>
   )
 }
