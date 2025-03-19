118b370c6b5bbbca4153dd14c3f59f383dd0621e
Kevin Grafstrom <kevin.grafstrom@gmail.com>
2025-03-18 20:27:57 -0400

    wip(vue-example): insert vue wrapper component library into vue-example

---
+++ examples/vue-example/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ChatUi} from '@chatkitty/vue'
+</script>
+
+<template>
+  <ChatUi msg="best player in the entire globe"/>
+</template>
+
+<style scoped>
+
+</style>
--- libraries/vue/vite.config.ts
+++ libraries/vue/vite.config.ts
@@ -1,29 +1,30 @@
 import { defineConfig } from 'vite';
 import { resolve } from "path";
 import vue from '@vitejs/plugin-vue';
+// import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      // src/indext.ts is where we have exported the component(s)
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "chatkitty-vue",
-      // the name of the output files when the build is run
-      fileName: (format) => `chatkitty-vue.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["vue"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: "Vue",
+    plugins: [vue()],
+    build: {
+        lib: {
+            // src/index.ts is where we have exported the component(s)
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "chatkitty-vue",
+            // the name of the output files when the build is run
+            fileName: (format) => `chatkitty-vue.${format}.js`
         },
-      },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ["vue"],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: "Vue",
+                },
+            },
+        },
     },
-  },
 })
