117a007cc79c0abb918f2cd5ce101759dde4ac39
Kevin Grafstrom <kevin.grafstrom@gmail.com>
2025-03-18 22:33:13 -0400

    wip(vue): call connectApi and loadChatUi functions in Vue SDK wrapper

--- libraries/vue/src/components/ChatUi.vue
+++ libraries/vue/src/components/ChatUi.vue
@@ -1,37 +1,56 @@
+import {connectApi, loadChatUi} from "chatkitty";
+
 <script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+import {connectApi, loadChatUi} from "chatkitty";
 
-const count = ref(0)
+const {apiKey, username, widgetId} = defineProps<{ apiKey: string, username: string, widgetId: string }>()
+
+const initializeChat = async () => {
+  const connection = await connectApi({
+    apiKey: apiKey,
+    username: username
+  });
+
+  const {user, unreadChannelsCount, notifications, updateUser} = connection;
+  console.log("Connected as user:", user.value);
+
+  user.watch((user) => console.log("User:", user));
+  unreadChannelsCount.watch((count) => console.log("Unread channels count:", count));
+  notifications.watch((notification) => console.log("Received notification:", notification));
+
+  loadChatUi(
+      {
+        widgetId: widgetId,
+        container: {height: "100%"},
+        audio: {enabled: true},
+        components: {
+          chat: (context) => ({
+            menuActions: [],
+            onMounted: () => {
+              console.log("Chat UI mounted with context:", context);
+              updateUser({properties: {lastUpdated: new Date().toISOString()}});
+            },
+            onHeaderSelected: (channel) => console.log(channel),
+            onMenuActionSelected: (action) => console.log(action)
+          })
+        },
+        templates: {}
+      },
+      {
+        timeout: 50000,
+        connection
+      }
+  );
+};
+
+initializeChat();
+
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div id="chat-ui"></div>
 </template>
 
 <style scoped>
