e9ed831c225125d97905259c42df95845f474a14
Kevin Grafstrom <kevin.grafstrom@gmail.com>
2025-03-19 12:08:42 -0400

    wip(vue): remove unnecessary console logs

--- libraries/vue/src/components/ChatUi.vue
+++ libraries/vue/src/components/ChatUi.vue
@@ -12,27 +12,16 @@ const initializeChat = async () => {
     username: username
   });
 
-  const {user, unreadChannelsCount, notifications, updateUser} = connection;
-  console.log("Connected as user:", user.value);
-
-  user.watch((user) => console.log("User:", user));
-  unreadChannelsCount.watch((count) => console.log("Unread channels count:", count));
-  notifications.watch((notification) => console.log("Received notification:", notification));
-
   loadChatUi(
       {
         widgetId: widgetId,
         container: {height: "100%"},
         audio: {enabled: true},
         components: {
-          chat: (context) => ({
+          chat: (_context) => ({
             menuActions: [],
             onMounted: () => {
-              console.log("Chat UI mounted with context:", context);
-              updateUser({properties: {lastUpdated: new Date().toISOString()}});
             },
-            onHeaderSelected: (channel) => console.log(channel),
-            onMenuActionSelected: (action) => console.log(action)
           })
         },
         templates: {}
