4f4d083bf15f5e530747d3beb11cae1bb493c865
Kevin Grafstrom <kevin.grafstrom@gmail.com>
2025-03-21 15:15:09 -0400

    wip(angular): initialize angular wrapper component library

---
+++ libraries/angular/projects/chat-ui/src/library/chat-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ChatUi',
+  imports: [],
+  template: `
+    <p>
+      chat-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class ChatUi {
+
+}
---
+++ libraries/angular/projects/chat-ui/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of chat-ui
+ */
+
+export * from './library/chat-ui.service';
+export * from './library/chat-ui.component';
