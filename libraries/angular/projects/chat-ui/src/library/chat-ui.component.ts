7ad22c114b169d8e657c72e33ceae457da7608b5
Kevin Grafstrom <kevin.grafstrom@forma.ai>
2025-03-24 16:35:07 -0400

    wip(angular): create Angular wrapper SDK and an example for it (#22)
    * wip(angular): initialize angular wrapper component library
    
    * wip(angular-example): initialize angular example
    
    * wip(angular-example): incorporate angular wrapper component library into angular example
    
    * wip(angular): add phoenix app into angular wrapper component library
    
    * wip(angular): remove changes to pnpm-lock.yaml
    
    * wip(angular): add Angular wrapper component library to pnpm workspace
    
    * wip(angular): remove unnecessary initializeChat function
    
    * wip(angular): add main, module, types and exports to @chatkitty/angular package.json
    
    * wip(angular): fix mistake in @chatkitty/angular's package.json
    
    * wip(angular): remove main, module, types and exports from @chatkitty/angular's package.json
    
    * wip(angular): remove changes to pnpm-lock.yaml

---
+++ libraries/angular/projects/chat-ui/src/library/chat-ui.component.ts
@@ -0,0 +1,50 @@
+import {Component, Input} from '@angular/core';
+import {connectApi, loadChatUi} from "chatkitty";
+
+@Component({
+  selector: 'ChatUi',
+  imports: [],
+  template: `
+    <div id="chat-ui"></div>
+  `,
+  standalone: true,
+  styles: ``
+})
+export class ChatUi {
+
+  @Input({required: true}) apiKey: string = "";
+
+  @Input({required: true}) username: string = "";
+
+  @Input({required: true}) widgetId: string = "";
+
+
+  async ngOnInit() {
+
+    const connection = await connectApi({
+      apiKey: this.apiKey,
+      username: this.username
+    });
+
+    loadChatUi(
+      {
+        widgetId: this.widgetId,
+        container: {height: "100%"},
+        audio: {enabled: true},
+        components: {
+          chat: (_context) => ({
+            menuActions: [],
+            onMounted: () => {
+            },
+          })
+        },
+        templates: {}
+      },
+      {
+        timeout: 50000,
+        connection
+      }
+    );
+  }
+
+}
