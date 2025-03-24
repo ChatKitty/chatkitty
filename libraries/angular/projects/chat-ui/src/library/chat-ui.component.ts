7df17ae737b4960cdd7cf929172d9f75997193e8
Kevin Grafstrom <kevin.grafstrom@gmail.com>
2025-03-23 22:20:09 -0400

    wip(angular): add phoenix app into angular wrapper component library

--- libraries/angular/projects/chat-ui/src/library/chat-ui.component.ts
+++ libraries/angular/projects/chat-ui/src/library/chat-ui.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { connectApi, loadChatUi } from "chatkitty";
 
 @Component({
   selector: 'ChatUi',
   imports: [],
   template: `
-    <p>
-      chat-ui works!
-    </p>
+    <div id="chat-ui"></div>
   `,
+  standalone: true,
   styles: ``
 })
 export class ChatUi {
+
+  @Input({ required: true }) apiKey: string = "";
+
+  @Input({ required: true }) username: string = "";
+
+  @Input({ required: true }) widgetId: string = "";
+
+
+  async ngOnInit() {
+
+    const initializeChat = async () => {
+      const connection = await connectApi({
+        apiKey: this.apiKey,
+        username: this.username
+      });
+
+      loadChatUi(
+        {
+          widgetId: this.widgetId,
+          container: {height: "100%"},
+          audio: {enabled: true},
+          components: {
+            chat: (_context) => ({
+              menuActions: [],
+              onMounted: () => {
+              },
+            })
+          },
+          templates: {}
+        },
+        {
+          timeout: 50000,
+          connection
+        }
+      );
+    };
+
+    await initializeChat();
+  }
 
 }
