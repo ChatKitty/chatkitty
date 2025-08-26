e59153811a72b2592ea3553207e5482285d7ed72
Aaron Nwabuoku <aaron@chatkitty.com>
2025-08-26 11:52:39 -0400

    fix(react): use onDispose to preserve API connection disposal logic

--- libraries/react/src/api/index.ts
+++ libraries/react/src/api/index.ts
@@ -1,27 +1,23 @@
-import { useMemo, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { connectApi, ConnectApiOptions, ApiConnection } from '@chatkitty/core';
 
 export const useApiConnection = (setup: (connection: ApiConnection) => void, options: ConnectApiOptions) => {
-	const loading = useRef(false);
-
 	const [connection, setConnection] = useState<ApiConnection>();
 
-	useMemo(() => {
-		if (loading.current) return;
+	const loading = useRef(false);
 
+	useEffect(() => {
 		if (connection) return;
+
+		if (loading.current) return;
 
 		const initializeApiConnection = async () => {
 			const value = await connectApi(options);
 
-			const superDispose = value.dispose.bind(value);
-
-			value.dispose = async () => {
-				await superDispose();
-
+			value.onDispose(async () => {
 				setConnection(undefined);
-			};
+			});
 
 			setConnection(value);
 
